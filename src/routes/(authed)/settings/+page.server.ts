d0f4b0d8456e4bde7e1e32d1e3106765731ebaa7
Elijah Crum <elijah@crums.us>
2026-06-28 17:21:11 -0500

    Add material creation validation

--- src/routes/(authed)/settings/+page.server.ts
+++ src/routes/(authed)/settings/+page.server.ts
@@ -1,6 +1,42 @@
-import type { PageServerLoad } from './$types';
+import { resolve } from '$app/paths';
+import { requireUser } from '$lib/server/auth';
+import { db } from '$lib/server/db';
+import { material } from '$lib/server/db/schema';
+import type { Actions, PageServerLoad } from './$types';
+import { fail } from '@sveltejs/kit';
+import { superValidate } from 'sveltekit-superforms';
+import { zod4 } from 'sveltekit-superforms/adapters';
+import z from 'zod';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	const user = locals.user;
 	return { user };
+};
+
+const newMaterialSchema = z.object({
+	name: z.string().min(1),
+	onshapeName: z.string().optional()
+});
+
+export type NewMaterialSchema = typeof newMaterialSchema;
+
+export const actions: Actions = {
+	createMaterial: async (event) => {
+		requireUser(event.locals, resolve('/settings'));
+
+		const form = await superValidate(event, zod4(newMaterialSchema));
+		if (!form.valid) {
+			return fail(400, {
+				form
+			});
+		}
+
+		try {
+			db.insert(material).values({ name: form.data.name });
+		} catch {
+			return fail(500, 'Failed to create material');
+		}
+
+		return { form };
+	}
 };
