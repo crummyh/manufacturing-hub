e302ee061579f278603c61e6bcb6794ea0afa12f
Elijah Crum <elijah@crums.us>
2026-09-01 20:21:22 -0500

    Add firsty as logo

--- src/lib/components/main-logo.svelte
+++ src/lib/components/main-logo.svelte
@@ -1,1 +1,14 @@
-<div class="bg-primary size-8 rounded-lg"></div>
+<script lang="ts">
+    import firsty from '$lib/assets/firsty.svg';
+	import type { HTMLImgAttributes } from 'svelte/elements';
+
+    interface Props extends HTMLImgAttributes {
+      size?: number
+    }
+
+    let { size = 8, class: divClass, ...restProps }: Props = $props()
+</script>
+
+<div class={["dark:bg-white dark:rounded-lg dark:p-1", divClass]}>
+    <img class="size-{size}" src={firsty} alt="firsty logo" {...restProps} />
+</div>
